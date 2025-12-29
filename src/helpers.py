6ced82c505a73304cc779646d7a0a02e741c5b4d
Andres Rios <riosandres294@gmail.com>
2025-12-29 11:45:29 -0500

    tidying up

--- src/helpers.py
+++ src/helpers.py
@@ -1,4 +1,21 @@
+import pandas as pd
+
+path = "../data/Animes.csv"
+
 
 # compouned annual growth rate
 def cagr(start_value, end_value, years):
     return (end_value / start_value) ** (1 / years) - 1
+
+# create data frame df and isekai_df and return both of them
+def cidf():
+    df = pd.read_csv(path, usecols=["Title", "Release", "Theme"])
+
+    df = df.dropna(subset=["Theme"])
+
+    df["Release"] = pd.to_datetime(df["Release"], format="mixed")
+    df = df[df["Release"].dt.year > 1999]
+
+    isekai_df = df[df["Theme"].str.contains("isekai", case=False, na=False)]
+
+    return df, isekai_df
